595d41da5c7d9a7e2ba1c7ad45cd3bcdb75c63ac
alesha reed <aleshak83@gmail.com>
2019-09-22 21:17:58 -0500

    added input box and variable and styling

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ console.log('temperature converter')
 const printToDom = (divId, toPrint) => {
     document.getElementById(divId).innerHTML = toPrint;
 };
+
+let userTemp = document.getElementById('tempcomment').value;
+
